72109d7cf6e6c2faa89e225a6ac5e64bb2c7a7a2
lijian <lijian@linkv.sg>
2020-08-20 21:41:52 +0800

    1.新增音视频auth功能 2.example增加音视频用例

--- linkv_sdk/rtc/rtc.py
+++ linkv_sdk/rtc/rtc.py
@@ -1,13 +1,31 @@
 # -*- coding: UTF-8 -*-
 
 from .config import config
+import time
+import hmac
+from datetime import datetime
 
 
 class Rtc(object):
     def __init__(self):
         pass
 
+    @staticmethod
+    def GenAuth():
+        now = str(int(time.mktime(datetime.now().timetuple())))
+        data = '{}{}'.format(config().app_id, now)
+        auth_mac = hmac.new(config().app_key.encode('utf-8'), data.encode('utf-8'), digestmod='SHA1')
+        return {
+            'status': True,
+            'app_id': config().app_id,
+            'auth': auth_mac.hexdigest(),
+            'expire_ts': now,
+        }
+
 
 class LvRTC(Rtc):
     def __init__(self):
         Rtc.__init__(self)
+
+    def GenAuth(self):
+        return super(LvRTC, self).GenAuth()
